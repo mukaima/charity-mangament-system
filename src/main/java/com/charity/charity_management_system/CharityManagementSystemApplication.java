b1efb2c2a14f8cb7b74453fb4ae275d2e17c2962
Mohamed Mekhemer <mmekhemer3@gmail.com>
2024-08-19 02:56:22 +0300

    first commit

---
+++ src/main/java/com/charity/charity_management_system/CharityManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.charity.charity_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharityManagementSystemApplication.class, args);
+	}
+
+}
